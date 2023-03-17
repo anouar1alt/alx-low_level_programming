7ee4b6606976a9b4d93688612f0553884315bfb9
Anouar Mobsitte <mobsitteanouar059@gmail.com>
2023-03-16 17:01:20 -0700

    toto

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
- *  * main - a function that prints a given input within puts
- *   * Return: 0 - (Success)
- *    */
+ *main - a function that prints a given input within puts
+ *Return: 0 - (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
